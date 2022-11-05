8490e90783f241067cbac76e2cf5ea3c655bd624
Thorsten Luenborg <t.luenborg@googlemail.com>
2022-11-05 10:19:45 +0100

    tests: playground file to manually check out TS support & inference

---
+++ src/typeTest.ts
@@ -0,0 +1,54 @@
+/**
+ * This file is not to be used anywhere, it's just to test the type inference
+ */
+import { defineComponent } from 'vue'
+import { createComposableFromMixin } from './createComposable'
+import { defineMixin } from './defineMixin'
+
+interface User {
+  name: string
+  street: string
+  city: string
+  hobbies: string[]
+}
+
+const mixin = defineMixin({
+  props: {},
+  data: () => ({
+    msg: 'Hello World',
+    age: 10,
+    user: {} as User,
+  }),
+  computed: {
+    birthYear(): number {
+      return new Date().getFullYear() - this.age
+    },
+  },
+  methods: {
+    testFn(): void {
+      console.log(this.user)
+    },
+  },
+} as const)
+
+const testComposable = createComposableFromMixin(mixin)
+
+const Comp = defineComponent({
+  setup() {
+    const state = testComposable()
+
+    state.age
+    state.msg
+    state.user.value
+    state.birthYear
+    state.testFn()
+  },
+})
+
+const CompWMixin = defineComponent({
+  mixins: [mixin],
+})
+
+type TCompWMixin = InstanceType<typeof CompWMixin>
+type checkAge = TCompWMixin['age']
+type checkUser = TCompWMixin['user']
