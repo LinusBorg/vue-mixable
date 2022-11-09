18d92d6eb1a073497ac40990b42f5c5b4d93883e
ufec <1750367868@qq.com>
2022-11-09 23:20:21 +0800

    fix: merge import type

--- src/createComposable.ts
+++ src/createComposable.ts
@@ -20,8 +20,6 @@ import {
   watch,
   reactive,
   provide,
-  type ComponentPublicInstance,
-  type WatchCallback,
 } from 'vue'
 import { callHook, isArray, isFunction, isObject, isString } from './utils'
 import { createContextProxy } from './vmContextProxy'
@@ -37,6 +35,8 @@ import type {
   ComponentPropsOptions,
   ExtractDefaultPropTypes,
   EmitsOptions,
+  ComponentPublicInstance,
+  WatchCallback,
 } from 'vue'
 
 // import { cache } from './cache'
